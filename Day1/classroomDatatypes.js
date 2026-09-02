458462652d3cfd2e63b5a4e2abf7ee1f7a7c193e
Madhusree95 <sairigapumadhusree9295@gmail.com>
2026-09-02 17:24:12 +0530

    Add files via upload

---
+++ Day1/classroomDatatypes.js
@@ -0,0 +1,20 @@
+
+var firstName = "Madhusree"
+console.log (firstName)
+console.log (typeof firstName)
+
+var companyName = 'TechM'
+console.log (companyName)
+console.log (typeof companyName)
+
+var mobileNumber = 9876543210
+console.log (mobileNumber)
+console.log (typeof mobileNumber)
+
+var isAutomation = true
+console.log (isAutomation)
+console.log (typeof isAutomation) 
+
+var hasPlaywright
+console.log (hasPlaywright)
+console.log (typeof hasPlaywright)
---
+++ Day1/homeDatatypes.js
@@ -0,0 +1,19 @@
+let userName = "Madhusree"
+console.log (userName)
+console.log (typeof userName)
+
+let organization = 'TechM'
+console.log (organization)
+console.log (typeof organization)
+
+let contactNumber = 9876543210
+console.log (contactNumber)
+console.log (typeof contactNumber)
+
+let knowsAutomation = true
+console.log (knowsAutomation)
+console.log (typeof knowsAutomation) 
+
+let usesPlaywright
+console.log (usesPlaywright)
+console.log (typeof usesPlaywright)
