ee9391976048a8496309e1c2b281bee49105612c
Madhusree95 <sairigapumadhusree9295@gmail.com>
2026-09-09 14:25:32 +0530

    comments added
    Added comments to clarify the purpose of each test case.

--- W2Day2/homework-launch-browser.spec.ts
+++ W2Day2/homework-launch-browser.spec.ts
@@ -1,11 +1,11 @@
-import {test, expect} from "@playwright/test"
+import {test} from "@playwright/test"
 test('launch redbus',async ({page})=>{
-    await page.goto( "https://www.redbus.in" );
+    await page.goto( "https://www.redbus.in" );//launching redbus
     console.log(await page.title());
     console.log(await page.url());
 })
 test('launch flipkart',async ({page})=>{
-    await page.goto( "https://www.flipkart.com" );
+    await page.goto( "https://www.flipkart.com" );//launchinf flipkart
     console.log(await page.title());
     console.log(await page.url());
 });
