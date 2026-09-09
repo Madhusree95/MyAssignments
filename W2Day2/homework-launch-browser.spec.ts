0931b276f371632380bef8c7e0840975d1db186e
Madhusree95 <sairigapumadhusree9295@gmail.com>
2026-09-09 14:27:27 +0530

    Refactor tests to include 'expect' from Playwright

--- W2Day2/homework-launch-browser.spec.ts
+++ W2Day2/homework-launch-browser.spec.ts
@@ -1,4 +1,4 @@
-import {test} from "@playwright/test"
+import {test,expect} from "@playwright/test"
 test('launch redbus',async ({page})=>{
     await page.goto( "https://www.redbus.in" );//launching redbus
     console.log(await page.title());
